54507c93cf2ce95ff10c7afe02cdf294c015db07
vilyanaangelova <vilyana.angelova@akquinet.de>
2023-07-30 18:15:13 +0200

    added 404 error page

--- JobPortal-CourseProject/JobPortal.Web/Controllers/HomeController.cs
+++ JobPortal-CourseProject/JobPortal.Web/Controllers/HomeController.cs
@@ -17,9 +17,14 @@
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            
+            return View();
         }
     }
 }
