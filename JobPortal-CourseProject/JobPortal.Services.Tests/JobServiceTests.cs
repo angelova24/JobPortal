3ecadc1394530a3bf031aad938e46aa82adc2601
vilyanaangelova <vilyana.angelova@akquinet.de>
2023-08-11 16:56:40 +0200

    added more unit test

--- JobPortal-CourseProject/JobPortal.Services.Tests/JobServiceTests.cs
+++ JobPortal-CourseProject/JobPortal.Services.Tests/JobServiceTests.cs
@@ -1,6 +1,7 @@
 namespace JobPortal.Services.Tests
 {
     using JobPortal.Data;
+    using JobPortal.Data.Models;
     using Microsoft.EntityFrameworkCore;
     using Sevices.Data;
     using Web.ViewModels.Job;
@@ -28,7 +29,7 @@ namespace JobPortal.Services.Tests
         public async Task CreateAndReturnIdAsyncShouldCreateJobAndReturnId()
         {
             const string employerId = "aa294071-5c7d-4f4c-8451-42fac506957c";
-            var model = new JobAddFormModel()
+            var model = new JobAddFormModel
             {
                Title = "Some Title",
                Description = "Lorem ipsum dolor sit amet, consetetur sadipscing",
@@ -84,7 +85,7 @@ namespace JobPortal.Services.Tests
         {
             var existingJob = await dbContext.Jobs.FirstOrDefaultAsync();
 
-            var model = new JobAddFormModel()
+            var model = new JobAddFormModel
             {
                 Title = "Some Title",
                 Description = "Lorem ipsum dolor sit amet, consetetur sadipscing",
@@ -113,6 +114,99 @@ namespace JobPortal.Services.Tests
             await jobService.DeleteJobByIdAsync(existingJob?.Id.ToString()!);
             
             Assert.That(existingJob?.DeletedOn, Is.Not.Null);
+        }
+        
+        [Test]
+        public async Task ApplicationExistsAsyncShouldReturnTrueWhenExists()
+        {
+            var user = new ApplicationUser
+            {
+                Id = Guid.Parse("9fd4a332-1e0e-4e07-8011-d32c19d8c3e0"),
+                Email = "someUser@test.com",
+                UserName = "someUser@test.com",
+                NormalizedUserName = "someUser@test.com",
+                NormalizedEmail = "someUser@test.com",
+                SecurityStamp = "714393af-a207-4356-aac2-c75b13315a96",
+                FirstName = "Test",
+                LastName = "User"
+            };
+            
+            await dbContext.Users.AddAsync(user);
+            var job = await dbContext.Jobs.FirstAsync();
+            
+            var model = new UserJobs
+            {
+                CandidateId = user.Id,
+                JobId = job.Id,
+                FilePath = "Some/Path"
+            };
+            job.Candidates.Add(model);
+            await dbContext.SaveChangesAsync();
+            
+            var result = await jobService.ApplicationExistsAsync(job.Id.ToString(), user.Id.ToString());
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public async Task ApplicationExistsAsyncShouldReturnFalseWhenNotExists()
+        {
+            var jobId = Guid.NewGuid().ToString();
+            var candidateId = Guid.NewGuid().ToString();
+            
+            var result = await jobService.ApplicationExistsAsync(jobId, candidateId);
+
+            Assert.That(result, Is.False);
+        }
+        
+        [Test]
+        public async Task GetCvPathAsyncShouldReturnPath()
+        {
+            var user = new ApplicationUser
+            {
+                Id = Guid.Parse("9fd4a332-1e0e-4e07-8011-d32c19d8c3e0"),
+                Email = "someUser@test.com",
+                UserName = "someUser@test.com",
+                NormalizedUserName = "someUser@test.com",
+                NormalizedEmail = "someUser@test.com",
+                SecurityStamp = "714393af-a207-4356-aac2-c75b13315a96",
+                FirstName = "Test",
+                LastName = "User"
+            };
+            
+            await dbContext.Users.AddAsync(user);
+            var job = await dbContext.Jobs.FirstAsync();
+            
+            var model = new UserJobs
+            {
+                CandidateId = user.Id,
+                JobId = job.Id,
+                FilePath = "Some/Path"
+            };
+            job.Candidates.Add(model);
+            await dbContext.SaveChangesAsync();
+            
+            var result = await jobService.GetCvPathAsync(job.Id.ToString(), user.Id.ToString());
+
+            Assert.That(result, Is.EqualTo(model.FilePath));
+        }
+        
+        [Test]
+        public async Task GetAllJobsAsyncShouldReturnJobs()
+        {
+            var result = await jobService.GetAllJobsAsync(new JobsQueryModel());
+
+            Assert.That(result, Is.Not.Null);
+        }
+        
+        [Test]
+        public async Task GetJobDetailsByIdAsyncShouldReturnJob()
+        {
+            var job = await dbContext.Jobs.FirstAsync();
+            
+            var result = await jobService.GetJobDetailsByIdAsync(job.Id.ToString());
+
+            Assert.That(result, Is.Not.Null);
         }
     }
 }
